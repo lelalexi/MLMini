80628e86b7a5257353f7e908d88542c605381af2
lelalexi <alexisgarcia406@gmail.com>
2019-09-23 11:42:05 -0300

    New class added to extra to manage the loading animation (spinning loading wheel)

---
+++ MLMini/Extras/SpinnerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SpinningWheel.swift
+//  MLMini
+//
+//  Created by Gaston Alexis Garcia Carli on 23/09/2019.
+//  Copyright © 2019 ale. All rights reserved.
+//
+
+import UIKit
+
+class SpinnerViewController: UIViewController {
+    var spinner = UIActivityIndicatorView(style: .whiteLarge)
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.7)
+        
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.startAnimating()
+        view.addSubview(spinner)
+        
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
