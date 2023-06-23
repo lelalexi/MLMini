bbc408eb26e04e0012234155402fae0fc817acb7
Alexis Garcia Carli <>
2023-06-23 14:10:58 -0300

    Adds missing localizables for MainDetailCell

--- MLMini/Resources/Localization/MLLocalizables.swift
+++ MLMini/Resources/Localization/MLLocalizables.swift
@@ -18,6 +18,11 @@ struct MLLocalizables {
         static var freeShippingLabel: String { "FreeShippingLabel".localized() }
     }
     
+    struct ProductDetailView {
+        static var soldItemsLabel: String { "SoldItemsLabel".localized() }
+        static var soldByItemsLabel: String { "SoldByItemsLabel".localized() }
+    }
+    
     struct ComponentFeedbackView {
         // Not Found
         static var notFoundTitle: String { "FeedbackNotFoundTitle".localized() }
